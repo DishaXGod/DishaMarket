dc9bd7036926dd68193172d62f1457605a6bdc10
DishaXGod <64671053+DishaXGod@users.noreply.github.com>
2020-05-04 04:47:48 +0300

    Update terminal.lua

--- terminal.lua
+++ terminal.lua
@@ -17,7 +17,7 @@ local me_side = "DOWN"
 local pim_side = "UP"
 local server = "Default"
 local version, port = "modem", 1414
-local serverAddress = "f46477ae-2147-4c9e-b63f-00946bb8de36"
+local serverAddress = "d4a82811-ce76-4f92-a76f-d7872c57d77c"
 
 local priceLottery = 150
 local superPrize = 10000

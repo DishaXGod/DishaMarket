d5d749c01111a6937f1b531136bd32c6458edbee
DishaXGod <64671053+DishaXGod@users.noreply.github.com>
2020-05-05 08:49:00 +0300

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,7 +1,7 @@
 {
     shop = {
+		{text = "Ядро Виверны", buyPrice = 2500000, minCount = 0, maxCount = 15000, fingerprint = {{dmg=0.0,id="DraconicEvolution:wyvernCore"}}, raw_name = {"item.draconicevolution:wyverncore"}},
 		{text = "Жемчуг Эндера", buyPrice = 5000, sellPrice = 4000, minCount = 0, maxCount = 50000, fingerprint = {{dmg=0.0,id="minecraft:ender_pearl"}}, raw_name = {"item.enderpearl"}},
-		{text = "Ядро Виверны", buyPrice = 2500000, minCount = 0, maxCount = 15000, fingerprint = {{dmg=0.0,id="DraconicEvolution:wywrnCore"}}, raw_name = {"item.draconicevolution:wywerncore"}},
 		{text = "Ядро Дракона", buyPrice = 75000, minCount = 0, maxCount = 15000, fingerprint = {{dmg=0.0,id="DraconicEvolution:DraconicCore"}}, raw_name = {"item.draconicevolution:draconiccore"}},
 		{text = "Блок красного камня", sellPrice = 50, minCount = 0, maxCount = 50000, fingerprint = {{dmg=0.0,id="minecraft:redstone_block"}}, raw_name = {"redstone_block"}},
 		{text = "Иридий", buyPrice = 30000, minCount = 0, maxCount = 1310720, fingerprint = {{id="IC2:itemOreIridium",dmg=0.0}}, raw_name = {"ic2.itemoreiridium"}},

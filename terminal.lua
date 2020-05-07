2fe9e059a42dede43a01f9391b6c5722982b964b
DishaXGod <64671053+DishaXGod@users.noreply.github.com>
2020-05-07 22:08:37 +0300

    Update terminal.lua

--- terminal.lua
+++ terminal.lua
@@ -1022,9 +1022,9 @@ local function field(play)
     end
 end
 
-      local function lottery()
-	alert({"Если хочешь к нам пиши мне /m DishaXGod либо Airguy!"})
-end
+--      local function lottery()
+--	alert({"Если хочешь к нам пиши мне /m DishaXGod либо Airguy!"})
+--end
 --   balance(1)
 --   setColorText(nil, 3, "[0x68f029]Мгновенная беспроигрышная лотерея. Цена билета — [0xffffff]" .. priceLottery .. " [0x68f029]!", color.background)
 --     setColorText(19, 4, "[0x68f029]Супер-приз — [0xffffff]" .. superPrize .. " [0x68f029]SkillCoin!", color.background)

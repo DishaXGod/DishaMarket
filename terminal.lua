b30ead914a12556e01eb7e37b75ee7c00014a67b
DishaXGod <64671053+DishaXGod@users.noreply.github.com>
2020-05-07 19:26:19 +0300

    Update terminal.lua

--- terminal.lua
+++ terminal.lua
@@ -34,6 +34,17 @@ local INFO = [[
 [0x8B0000]7. [0xFFFFFF]Ты хочешь продать много товара, нажми "Весь инвентарь", а если парочку нажми "1 слот"
 [0x8B0000]8. [0xFFFFFF]Что будет, если я продам зачарованный(переименованный, заряженный, и т.д) меч/гравик и т.д? — цена таких вещей равняется стандартному предмету.
 ]]
+local INFO2 = [[
+[0x8B0000]DishaXGod говорит: [0xFFFFFF]Эй, спрячь ствол!
+[0x8B0000]1. [0xFFFFFF]Парень, 
+[0x8B0000]2. [0xFFFFFF]Это 
+[0x8B0000]3. [0xFFFFFF]Что такое [0x8B0000]SkillCoin[0xFFFFFF]? [0x8B0000]—[0xFFFFFF] Это валюта которой ты расплачиваешься в магазине.
+[0x8B0000]4. [0xFFFFFF]Как мне пополнить свой счет? [0x8B0000]—[0xFFFFFF] Зайди во вкладку "продажа" и продай что-то магазину.
+[0x8B0000]5. [0xFFFFFF]Как купить товар? [0x8B0000]—[0xFFFFFF] выбираете товар, набираете  товара, и товар будет добавлен в ваш инвентарь. Если денег недостаточно - товар нельзя купить.
+[0x8B0000]6. [0xFFFFFF]Как обменять руду? [0x8B0000]—[0xFFFFFF] Выбираете режим поиска предметов, и руда будет обменена на слитки автоматически!
+[0x8B0000]7. [0xFFFFFF]Ты хочешь продать много товара, нажми "Весь инвентарь", а если парочку нажми "1 слот"
+[0x8B0000]8. [0xFFFFFF]Что будет, если я продам зачарованный(переименованный, заряженный, и т.д) меч/гравик и т.д? — цена таких вещей равняется стандартному предмету.
+]]
 
 local pim, me, selector, tmpfs, modem = proxy("pim"), proxy("me_interface"), proxy("openperipheral_selector"), component.proxy(computer.tmpAddress())
 local json, serialization = require("json"), require("serialization")
@@ -1120,6 +1131,9 @@ end
 local function info()
     set(20, 1, "Тебе нужна помощь? Читай!", color.background, color.orange)
     drawInfo(1)
+local function info2()
+    set(20, 1, "Тебе нужна помощь? Читай!", color.background, color.orange)
+    drawInfo2(1)
 end
 
 local function drawFeedback(page)
@@ -1391,6 +1405,11 @@ function login(name)
                                 buttons.eula.notVisible = false
                                 buttons.back.notVisible = true
                                 toGui("info")
+                                  else
+                                buttons.eula.disabled = true
+                                buttons.eula.notVisible = false
+                                buttons.back.notVisible = true
+                                toGui("info2")
                             end
                         end
                     else
@@ -1501,7 +1520,7 @@ end
 
 buttons = {
     --Кнопки, которые отвечаю за перемещение по менюшкам
-    back = {buttonIn = {"shop", "buyItem", "sellItem", "other", "ore", "freeFood", "lottery", "account", "info", "feedbacks", "shop"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "   Назад   ", x = 25, y = 18, width = 11, height = 1, action = function() back() end},
+    back = {buttonIn = {"shop", "buyItem", "sellItem", "other", "ore", "freeFood", "lottery", "account", "info", "info2", "feedbacks", "shop"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "   Назад   ", x = 25, y = 18, width = 11, height = 1, action = function() back() end},
     backShop = {buttonIn = {"buy", "sell"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "   Назад   ", x = 31, y = 18, width = 11, height = 1, action = function() back() end},
     eula = {buttonIn = {"info"}, disabled = true, disabledBackground = color.blackGray, disabledForeground = color.blackOrange, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "  Я прочитал и соглашаюсь со всем  ", x = 13, y = 18, width = 35, height = 1, action = function() session.eula = true buttons.eula.notVisible = true buttons.back.notVisible = false requestWithData(nil, {method = "merge", toMerge = {eula = true}, name = session.name}) toGui("main") end},
     shop = {buttonIn = {"main"}, disabledBackground = color.blackGray, disabledForeground = color.blackLime, background = color.gray, activeBackground = color.blackGray, foreground = color.lime, activeForeground = color.blackLime, text = "Магазин", x = 19, y = 5, width = 24, height = 3, action = function() toGui("shop") end},
@@ -1517,6 +1536,7 @@ buttons = {
     freeFood = {buttonIn = {"other"}, disabledBackground = color.blackGray, disabledForeground = color.blackLime, background = color.gray, activeBackground = color.blackGray, foreground = color.lime, activeForeground = color.blackLime, text = "Бесплатный хавчик", x = 19, y = 8, width = 24, height = 3, action = function() toGui("freeFood") nextFood() end},
     -- lottery = {buttonIn = {"other"}, disabledBackground = color.blackGray, disabledForeground = color.blackLime, background = color.gray, activeBackground = color.blackGray, foreground = color.lime, activeForeground = color.blackLime, text = "Лотерея", x = 19, y = 12, width = 24, height = 3, action = function() toGui("lottery") end},
     alert = {buttonIn = {"alert"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "Назад", x = 26, y = 15, width = 9, height = 1, action = function() back() end},
+    info = {buttonIn = {"main"}, disabledBackground = color.background, disabledForeground = color.blackLime, background = color.background, activeBackground = color.background, foreground = color.lime, activeForeground = color.blackLime, text = "[Набор в NWA]", x = 27, y = 19, width = 8, height = 1, action = function() toGui("info2") end},
 
     zero = {buttonIn = {"buyItem"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "0", x = 29, y = 15, width = 3, height = 1, action = function() inputWrite("amount", 48) end},
     one = {buttonIn = {"buyItem"}, background = color.gray, activeBackground = color.blackGray, foreground = color.orange, activeForeground = color.blackOrange, text = "1", x = 24, y = 9, width = 3, height = 1, action = function() inputWrite("amount", 49) end},
@@ -1542,6 +1562,8 @@ buttons = {
 
     prevInfo = {buttonIn = {"info"}, disabled = true, disabledBackground = color.background, disabledForeground = color.blackBlue, background = color.background, activeBackground = background, foreground = color.blue, activeForeground = color.blackBlue, text = "<───", x = 21, y = 16, width = 4, height = 1, action = function() drawInfo(guiPage - 1) end},
     nextInfo = {buttonIn = {"info"}, disabled = true, disabledBackground = color.background, disabledForeground = color.blackBlue, background = color.background, activeBackground = background, foreground = color.blue, activeForeground = color.blackBlue, text = "───>", x = 36, y = 16, width = 4, height = 1, action = function() drawInfo(guiPage + 1) end},
+    prevInfo = {buttonIn = {"info2"}, disabled = true, disabledBackground = color.background, disabledForeground = color.blackBlue, background = color.background, activeBackground = background, foreground = color.blue, activeForeground = color.blackBlue, text = "<───", x = 21, y = 16, width = 4, height = 1, action = function() drawInfo(guiPage - 1) end},
+    nextInfo = {buttonIn = {"info2"}, disabled = true, disabledBackground = color.background, disabledForeground = color.blackBlue, background = color.background, activeBackground = background, foreground = color.blue, activeForeground = color.blackBlue, text = "───>", x = 36, y = 16, width = 4, height = 1, action = function() drawInfo(guiPage + 1) end},
     
     acceptFeedback = {buttonIn = {"feedbacks"}, notVisible = true, background = color.background, activeBackground = color.background, foreground = color.lime, activeForeground = color.blackLime, text = "[Подтвердить]", x = 24, y = 14, width = 13, height = 1, action = function() acceptFeedback() end},
     prevFeedback = {buttonIn = {"feedbacks"}, disabled = true, disabledBackground = color.background, disabledForeground = color.blackBlue, background = color.background, activeBackground = background, foreground = color.blue, activeForeground = color.blackBlue, text = "<───", x = 21, y = 16, width = 4, height = 1, action = function() drawFeedback(guiPage - 1) end},
